82dbd2a560d104df073c2f4a0349dee758a2f3c1
Oleksandr Panchuk <alekssen2008@gmail.com>
2025-05-10 23:57:20 +0300

    fix bugs

--- Database/Account.cs
+++ Database/Account.cs
@@ -9,7 +9,7 @@ namespace PasswordManager.Database
     public class Account
     {
         public int Id { get; set; }
-        public string? Username { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
         public bool Is2FAEnabled { get; set; } = false;
