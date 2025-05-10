d53db58bf788eadeb724c6d493cfec4185ed87b9
Admin <overgeardev@gmail.com>
2025-05-10 22:59:50 +0300

    Second commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Management;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace PasswordManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string password = "1234567890";
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MessageBox.Show(GetMotherboardSerialNumber());
+        }
+
+
+
+        string GetMotherboardSerialNumber()
+        {
+            var searcher = new ManagementObjectSearcher("SELECT SerialNumber FROM Win32_BaseBoard");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                return obj["SerialNumber"]?.ToString() ?? "Unknown";
+            }
+            return "Unknown";
+        }
+
+
+    }
+}
