256d45bfe17455ce802e15f48a362410f0417df7
Admin <overgeardev@gmail.com>
2025-05-14 17:25:41 +0300

    -Updated design -Color scheme changed

---
+++ UI/SearchBar.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PasswordManager.UI
+{
+    /// <summary>
+    /// Interaction logic for SearchBar.xaml
+    /// </summary>
+    public partial class SearchBar : UserControl
+    {
+        public SearchBar()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
